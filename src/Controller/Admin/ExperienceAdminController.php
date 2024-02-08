02270ea4963b32c87aa1e87d37eaf7d05912b5ea
Sébastien Petit <sebastienpetit27330@gmail.com>
2024-02-08 20:23:55 +0100

    update experience

--- src/Controller/Admin/ExperienceAdminController.php
+++ src/Controller/Admin/ExperienceAdminController.php
@@ -5,20 +5,32 @@ namespace App\Controller\Admin;
 use App\Entity\Experience;
 use App\Entity\Picture;
 use App\Form\ExperienceType;
+use App\Repository\ExperienceRepository;
 use App\Service\UploadProvider;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 #[Route('/admin/experience')]
 class ExperienceAdminController extends AbstractController
 {
     public function __construct(
         private UploadProvider $uploadProvider,
+        private ExperienceRepository $experienceRepository,
         private EntityManagerInterface $entityManager
     ){
+    }
+
+    #[Route('/list', methods: ['GET'])]
+    public function index(Request $request, NormalizerInterface $normalizer): JsonResponse
+    {
+        $experiences = $this->experienceRepository->findAll();
+        $data = $normalizer->normalize($experiences, 'json', ['groups' => 'experiences']);
+
+        return new JsonResponse($data);
     }
 
     #[Route('/new', methods: ['POST'])]
@@ -26,15 +38,15 @@ class ExperienceAdminController extends AbstractController
     {
         $experience = new Experience();
 
+        $form = $this->createForm(ExperienceType::class, $experience);
+        $form->submit($request->request->all());
+
         foreach ($request->files->all() as $file) {
             $fileName = $this->uploadProvider->upload($file);
             $picture = new Picture();
             $picture->setFileName($fileName);
             $experience->addPicture($picture);
         }
-
-        $form = $this->createForm(ExperienceType::class, $experience);
-        $form->submit($request->request->all());
 
         if ($form->isValid()) {
             $this->entityManager->persist($experience);
@@ -44,6 +56,50 @@ class ExperienceAdminController extends AbstractController
         }
 
         return new JsonResponse();
+    }
+
+    #[Route('/details/{id}', methods: ['GET'])]
+    public function show(Request $request, Experience $experience, NormalizerInterface $normalizer): JsonResponse
+    {
+        $data = $normalizer->normalize($experience, 'json', ['groups' => 'experience']);
+
+        $pictures = [];
+        foreach ($experience->getPictures() as $picture) {
+            $pictures[] = ['id' => $experience->getId(), 'url' => $request->getUriForPath('/images/') . $picture->getFileName()];
+        }
+        $data['pictures'] = $pictures;
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/update/{id}', methods: ['POST'])]
+    public function update(Request $request, Experience $experience, NormalizerInterface $normalizer): JsonResponse
+    {
+        $form = $this->createForm(ExperienceType::class, $experience);
+        $form->submit($request->request->all());
+
+        foreach ($request->files->all() as $file) {
+            $fileName = $this->uploadProvider->upload($file);
+            $picture = new Picture();
+            $picture->setFileName($fileName);
+            $experience->addPicture($picture);
+        }
+
+        if ($form->isValid()) {
+            $this->entityManager->flush();
+        } else {
+            return new JsonResponse($this->getErrorMessages($form));
+        }
+
+        $data = $normalizer->normalize($experience, 'json', ['groups' => 'experience']);
+
+        $pictures = [];
+        foreach ($experience->getPictures() as $picture) {
+            $pictures[] = ['id' => $experience->getId(), 'url' => $request->getUriForPath('/images/') . $picture->getFileName()];
+        }
+        $data['pictures'] = $pictures;
+
+        return new JsonResponse($data);
     }
 
     private function getErrorMessages($form): array
