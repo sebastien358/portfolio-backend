4919894b05be4369be28b54252600a0527d38cb5
Sébastien Petit <sebastienpetit27330@gmail.com>
2024-02-08 12:04:07 +0100

    reset password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RequestPasswordType;
+use App\Form\ResetPasswordType;
 use App\Form\UserType;
 use App\Service\MailerProvider;
 use DateTime;
@@ -54,6 +55,36 @@ class UserController extends AbstractController
         ])->getContent();
 
         $this->mailerProvider->sendEmail($user->getEmail(), 'Vous avez fait une demande de réinitialisation de mot de passe', $body);
+
+        return new JsonResponse();
+    }
+
+    #[Route('/reset-password/{token}', methods: ['POST'])]
+    public function resetPassword(Request $request, string $token): JsonResponse
+    {
+        $form = $this->createForm(ResetPasswordType::class);
+        $form->submit($request->request->all());
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['token' => $token]);
+        if (!$user) {
+            throw new \Exception('Le token n\'existe pas');
+        }
+
+        $interval = $user->getResetRequestAt()->diff(new DateTime('now'));
+        if ($interval->format('%h%') > 2) {
+            throw new \Exception('Votre demande est expiré, veuillez refaire une demande de réinitialisation de mot de passe');
+        }
+
+        if ($form->isValid()) {
+            $user->setPassword($this->userPasswordHasher->hashPassword(
+                $user,
+                $form->get('password')->getData()
+            ));
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        } else {
+            return new JsonResponse($this->getErrorMessages($form));
+        }
 
         return new JsonResponse();
     }
